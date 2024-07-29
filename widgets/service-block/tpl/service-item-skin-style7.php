d64583f6a5f804a22642768f5136fbaa2aedd23f
humaun124 <35164820+humaun124@users.noreply.github.com>
2024-07-29 16:30:35 +0600

    service7

---
+++ widgets/service-block/tpl/service-item-skin-style7.php
@@ -0,0 +1,26 @@
+<!-- Service Block Style1-->
+<?php
+$service_item['settings'] = $settings;
+$service_item['title_tag'] = $title_tag;
+$service_item['subtitle_tag'] = $subtitle_tag;
+$feature_link = $service_item['feature_link'];
+$count = $service_item['count'];
+$target = ( $feature_link && $feature_link['is_external'] ) ? ' target="_blank"' : '';
+$url = ( $feature_link && $feature_link['url'] ) ? $feature_link['url'] : '';
+?>
+<div class="service-item service-block-style7">
+	<div class="inner-box">
+		<div class="image-box">
+			<div class="image">
+				<?php mascot_core_carpento_get_shortcode_template_part( 'part-featured-image', null, 'service-block/tpl', $service_item, false );?>
+			</div>
+			<?php if ( $show_view_details_button == 'yes' ) : ?>
+				<a href="<?php echo esc_url( $feature_link['url'] );?>" class="icon"><i class="fa fa-long-arrow-alt-right"></i></a>
+			<?php endif; ?>
+		</div>
+		<div class="content-box">
+			<?php mascot_core_carpento_get_shortcode_template_part( 'part-subtitle', null, 'service-block/tpl', $service_item, false );?>
+			<?php mascot_core_carpento_get_shortcode_template_part( 'part-title', null, 'service-block/tpl', $service_item, false );?>
+		</div>
+	</div>
+</div>
